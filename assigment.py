3e15422f81fb52986e8f4735bc71f1b3725c7933
rushipanchal2 <85990721+rushipanchal2@users.noreply.github.com>
2021-07-28 13:56:34 +0530

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,36 @@
+Assigment 1 : Library managment system
+
+class Library:
+	def__init__(self,admin,barrower,book):
+	 self.admin = admin
+	 self.barrower = barrower
+	 self.book = book
+
+   class admin(self,view_book,add_book,edit_book,delete_book):
+   	   def Admin_login
+   	        pass 
+  	   def view_book(BookID):
+  	   	return(BookID,book)
+          def edit_book:
+                 pass
+          def History_A:
+                 pass
+          def lendbook:
+                   pass
+          def accept_return:
+                   pass
+
+
+
+   class barrower(self,register,user_login,Hisory_B):
+              def register:
+             	 pass
+             	def user_login
+             	 pass
+             	def Hisory_B 
+
+
+
+   class book(self,BookID,Book_title,Author_name,Total_pages,Avalible_copies,ISBN_no,Publish_year) 	 
+          booklist=[]
+          
